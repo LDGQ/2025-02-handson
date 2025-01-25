183996aca736a5388ef7a8a26793b3364ae57cb7
t-susaki <t.susaki.eng@gmail.com>
2025-01-25 17:59:24 +0900

    1つの商品が買える

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,82 @@
-import Image from "next/image";
+"use client";
+
+import React, {useMemo, useState} from "react";
+import Payment from "@/app/component/payment/page";
+import {Elements} from "@stripe/react-stripe-js";
+import {loadStripe} from "@stripe/stripe-js";
+import {useAuth} from "@/app/context/AuthContext";
+
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    const { customer } = useAuth();
+    const [clientSecret, setClientSecret] = useState('');   // 支払い情報
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    /** 商品を作成 **/
+    const products = [{
+            name: 'test',
+            currency: 'jpy',
+            amount: 100
+        }];
+
+    // 商品購入
+    const handlePurchase = () => {
+        createPaymentIntent(products[0].amount, products[0].currency, {}).then();
+    };
+
+    // 支払い情報を作成
+    const createPaymentIntent = async (amount: number, currency: string, metadata: object) => {
+        try {
+            const response = await fetch('/api/payment-intents', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ amount, currency, customer, metadata }),
+            });
+            const result = await response.json();
+            setClientSecret(result.client_secret);
+        } catch (error) {
+            console.error('Error creating payment intent:', error);
+        }
+    };
+
+    const pageContent = useMemo(() => {
+        // 支払い方法を定義
+        const paymentOptions = {
+            clientSecret,
+        };
+
+        // 支払い画面
+        if (clientSecret) {
+            return (
+                <Elements stripe={stripePromise} options={paymentOptions}>
+                    <Payment customerId={customer!.id} setClientSecret={setClientSecret} products={products} />
+                </Elements>
+            );
+        }
+
+        return (
+            <div className="flex flex-col items-center p-4 gap-2">
+                <div className="px-4 py-2">
+                    <button
+                        onClick={() => handlePurchase()}
+                        className="
+                        bg-blue-500 text-white font-semibold px-6 py-2 rounded shadow
+                        hover:bg-blue-600 focus:outline-none focus:ring-2
+                        focus:ring-blue-500 focus:ring-offset-2 transition duration-150 ease-in-out
+                        disabled:bg-gray-300 disabled:cursor-not-allowed
+                    "
+                    >
+                        購入
+                    </button>
+                </div>
+            </div>
+        );
+
+    }, [clientSecret, customer]);
+
+    return (
+        <div>
+            {pageContent}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    );
 }
