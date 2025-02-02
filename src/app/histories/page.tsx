5b17c0295bc5dec032447a3243d9cf28f93f95cc
t-susaki <t.susaki.eng@gmail.com>
2025-02-02 15:15:14 +0900

    返金処理

--- src/app/histories/page.tsx
+++ src/app/histories/page.tsx
@@ -49,6 +49,35 @@ const Histories: React.FC<HistoriesProps> = () => {
         }
     }, []);
 
+    // 返金処理
+    const refundsByChargeId = async (chargeId: string) => {
+        if(customer === null) return;
+        if (confirm('返金処理を行います。80%のみ返金されますが、大丈夫ですか？')) {
+            try {
+                setLoading(true);
+                // 返金処理を実行
+                await fetch('/api/refunds', {
+                    method: 'POST',
+                    headers: {'Content-Type': 'application/json'},
+                    body: JSON.stringify({chargeId: chargeId}),
+                });
+
+                // 商品情報を再取得
+                const response = await fetch(`/api/histories?customerId=${customer.id}`, {
+                    method: 'GET',
+                    headers: { 'Content-Type': 'application/json' },
+                })
+                const result = await response.json();
+                console.log({result: result.charges.data});
+                setCharges(result.charges.data);
+            } catch (error) {
+                console.error('エラーが発生しました:', error);
+            } finally {
+                setLoading(false);
+            }
+        }
+    }
+
     const fetchProducts = (id: string) => {
         if(products.length === 0) return;
         return products.find((product) => product.id === id);
@@ -70,6 +99,7 @@ const Histories: React.FC<HistoriesProps> = () => {
             <tr className="bg-gray-100">
                 <th className="border border-gray-300 px-4 py-2">合計金額</th>
                 <th className="border border-gray-300 px-4 py-2">状態</th>
+                <th className="border border-gray-300 px-4 py-2">返金</th>
                 <th className="border border-gray-300 px-4 py-2">詳細</th>
             </tr>
             </thead>
@@ -78,6 +108,23 @@ const Histories: React.FC<HistoriesProps> = () => {
                 <tr key={charge.id} className="border-b border-gray-300">
                     <td className="border border-gray-300 px-4 py-2">{charge.amount} {charge.currency === 'jpy' ? '円' : 'USD'}</td>
                     <td className="border border-gray-300 px-4 py-2">{charge.status == 'succeeded' ? "成功" : charge.status}</td>
+                    <td className="border border-none px-4 py-2">
+                        <div className='flex justify-between items-center'>
+                            <p>{charge.amount_refunded == 0 ? 'なし' : 'あり'}</p>
+                            <p>
+                                {charge.amount_refunded != 0 ?
+                                    <>({charge.amount_refunded}{charge.currency === 'jpy' ? '円' : 'USD'})</> :
+                                    <>
+                                        <button
+                                            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                                            onClick={() => {
+                                                refundsByChargeId(charge.id).then();
+                                            }}>返金
+                                        </button>
+                                    </>
+                                }</p>
+                        </div>
+                    </td>
                     <td className="border border-gray-300 px-4 py-2">
                         {Object.keys(charge.metadata).map((key) => {
                             const product = fetchProducts(key);
